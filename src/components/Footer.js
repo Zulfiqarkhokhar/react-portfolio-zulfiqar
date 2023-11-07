793fdfe41231e4aa2bcdc1c54cab7eb44ddf1821
ZulfiqarAli <119655783+Zulfiqarkhokhar@users.noreply.github.com>
2023-11-07 12:22:19 +0500

    Footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,13 @@
-export default function Footer(){
-    return<>
-        <h1>
-            Footer
-        </h1>
-    </>
-}
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="bg-dark text-light text-center p-3">
+            <div className="container">
+                <p>&copy; 2023 Your Company Name</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,32 +1,62 @@
 import Card from "react-bootstrap/Card";
 import Image from "react-bootstrap/Image";
-import {Badge} from "react-bootstrap";
+import {Badge, Container} from "react-bootstrap";
 import Buyer from "../assets/images/buyer.jpg"
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export const BuyerReview = ()=>{
     const reviews=
+        [
         {
             buyerImage: Buyer,
             buyerName: "John Doe",
             stars: 5,
             feedback: "Great product! I love it.",
-        };
+        },
+            {
+                buyerImage: Buyer,
+                buyerName: "Yasir Ali",
+                stars: 3,
+                feedback: "Great product! I love it.",
+            },
+            {
+                buyerImage: Buyer,
+                buyerName: "Talabdar",
+                stars: 4.5,
+                feedback: "Great product! I love it.",
+            },
+        ];
         return (
-            <Card>
-                <Card.Body>
-                    <Image src={reviews.buyerImage} className="rounded-circle"/>
-                    <Card.Title>{reviews.buyerName}</Card.Title>
-                    <Badge bg="info">Rating: {reviews.stars}/5</Badge>
-                    <Card.Text>{reviews.feedback}</Card.Text>
-                </Card.Body>
-            </Card>
+
+
+            <>
+                <h1 className="d-flex justify-content-center align-items-center">Buyer Reviews</h1>
+    <Container className='py-5'>
+        <Row xs={1} md={2} className="g-4">
+            {reviews.map((reviews) =>{
+                return (
+                <Col className="col-lg-4 col-md-6 col-sm-12">
+                    <Card>
+                        <Card.Body>
+                            <Image src={reviews.buyerImage}/>
+                            <Card.Title>{reviews.buyerName}</Card.Title>
+                            <Badge bg="info">Rating: {reviews.stars}/5</Badge>
+                            <Card.Text>{reviews.feedback}</Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                )
+            })}
+        </Row>
+    </Container>
+            </>
         );
 }
 
 export const SellerReview = ()=>{
     return(
-        <>
-        <h1>Seller Reviews</h1>
-        </>
+        <></>
+
     )
 }
